4dbbe9688d7bf23f5d249d67d7c495f5f02056b6
SocioPhobe0 <annauhimuk@gmail.com>
2025-06-09 00:42:50 +0300

    ффффффф

--- Program.cs
+++ Program.cs
@@ -4,30 +4,40 @@ using MyPresentationApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews(); 
+// Конфигурация сервисов
+builder.Services.AddControllersWithViews();
 
-// Получаем строку подключения
-var host = builder.Configuration["DB_HOST"];
-var port = builder.Configuration["DB_PORT"];
-var dbName = builder.Configuration["DB_NAME"];
-var dbUser = builder.Configuration["DB_USER"];
-var dbPassword = builder.Configuration["DB_PASSWORD"];
-
-var connectionString = $"Host={host};Port={port};Database={dbName};Username={dbUser};Password={dbPassword}";
-
-builder.Services.AddDbContext<AppDbContext>(options =>
+// Настройка БД
+var connectionString = GetConnectionString(builder.Configuration);
+builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseNpgsql(connectionString));
 
-
+// Другие сервисы
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<PresentationService>();
 builder.Services.AddScoped<ElementService>();
 builder.Services.AddScoped<SlideService>();
 
-
 var app = builder.Build();
 
+// Применение миграций ДО настройки middleware
+try 
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        db.Database.Migrate();
+    }
+}
+catch (Exception ex)
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "Migration failed!");
+    throw; // Прерываем запуск приложения при ошибке миграции
+}
+
+// Настройка middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -40,34 +50,27 @@ else
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
-
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-// Автомиграции (применение миграций при запуске)
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        var db = services.GetRequiredService<AppDbContext>();
-        db.Database.Migrate(); // Применяем миграции
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred while migrating the database.");
-    }
-}
-
 app.Run();
+
+string GetConnectionString(IConfiguration config)
+{
+    var host = config["DB_HOST"];
+    var port = config["DB_PORT"];
+    var dbName = config["DB_NAME"];
+    var dbUser = config["DB_USER"];
+    var dbPassword = config["DB_PASSWORD"];
+    
+    // Логирование для отладки (удалите в продакшене)
+    Console.WriteLine($"Connecting to: Host={host};Port={port};Database={dbName};Username={dbUser}");
+    
+    return $"Host={host};Port={port};Database={dbName};Username={dbUser};Password={dbPassword}";
+}
